873495b66782197b5c98042f84d0f49cdc58571b
atagadtad <aarontagadtad@gmail.com>
2019-09-03 13:52:29 +0000

    2nd day initial commit of any changes from the day before

--- express_server.js
+++ express_server.js
@@ -8,13 +8,16 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.ggogle.com"
+  "9sm5xK": "http://www.google.com"
+
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// when asked for /urls.json in the browser, server responeds with the
+// .json of the urlDatabase object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -30,8 +33,15 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  res.send("Ok");
-})
+  let temporaryString = generatedRandomString()
+  urlDatabase[temporaryString] = req.body.longURL;
+  res.redirect(`/urls/${temporaryString}`)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -60,7 +70,3 @@ function generatedRandomString() {
   }
   return sixString;
 };
-
-
-
-console.log(generatedRandomString());
